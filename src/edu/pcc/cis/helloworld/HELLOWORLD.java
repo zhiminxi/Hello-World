d66f1d046aebef562ae882ce02ba4cae6241c862
zhimin.xi <zhimin.xi@pcc.edu>
2018-05-01 16:38:56 -0700

    I add another ! in my statement

--- src/edu/pcc/cis/helloworld/HELLOWORLD.java
+++ src/edu/pcc/cis/helloworld/HELLOWORLD.java
@@ -2,6 +2,6 @@ package edu.pcc.cis.helloworld;
 
 public class HELLOWORLD {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!!");
     }
 }
